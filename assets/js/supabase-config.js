4f9a5bbdda374e7332034a54f60cff70724981e0
userofficial1976-afk <userofficial1976@gmail.com>
2026-08-02 13:50:16 +0800

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -6,10 +6,11 @@
 
 const SUPABASE_URL = "https://qoetisgoqcpdekxusbnu.supabase.co";
 
+
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZXRpc2dvcWNwZGVreHVzYm51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NDQ3NjksImV4cCI6MjEwMTIyMDc2OX0.-HFGuRgwfobOZ3YOa-7RXYdRkykbnyzD_eGwJ_3m9uU";
 
 
-const supabaseClient = supabase.createClient(
+window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );

a3a444c4bad1a7ecd59d6a15ea06afd80608abea
userofficial1976-afk <userofficial1976@gmail.com>
2026-08-19 14:40:09 +0800

    Update rk02-laporan-ptw.js

--- rk02-laporan-ptw.js
+++ rk02-laporan-ptw.js
@@ -7,19 +7,7 @@
 // SUPABASE
 // =====================================================
 
-const SUPABASE_URL =
-    "https://qoetisgoqcpdekxusbnu.supabase.co";
 
-
-const SUPABASE_ANON_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZXRpc2dvcWNwZGVreHVzYm51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NDQ3NjksImV4cCI6MjEwMTIyMDc2OX0.-HFGuRgwfobOZ3YOa-7RXYdRkykbnyzD_eGwJ_3m9uU";
-
-
-const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_ANON_KEY
-    );
 
 
 // =====================================================
